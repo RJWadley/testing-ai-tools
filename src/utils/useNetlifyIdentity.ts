8ed8ea4a451f42c7d8ba04543bd64763b553e4df
Robbie <robbiethewadley@gmail.com>
2023-04-28 15:54:49 -0600

    SSR fix

--- src/utils/useNetlifyIdentity.ts
+++ src/utils/useNetlifyIdentity.ts
@@ -1,3 +1,4 @@
+import { isBrowser } from "library/functions"
 import netlifyIdentity from "netlify-identity-widget"
 import { useEffect, useState } from "react"
 
@@ -18,22 +19,24 @@ const values: {
   token: null,
 }
 
-netlifyIdentity.on("login", newUser => {
-  values.isAuthenticated = true
-  values.token = newUser.token?.access_token ?? null
-  values.user = newUser
-})
+if (isBrowser()) {
+  netlifyIdentity.on("login", newUser => {
+    values.isAuthenticated = true
+    values.token = newUser.token?.access_token ?? null
+    values.user = newUser
+  })
 
-netlifyIdentity.on("logout", () => {
-  values.isAuthenticated = false
-  values.token = null
-  values.user = null
-})
+  netlifyIdentity.on("logout", () => {
+    values.isAuthenticated = false
+    values.token = null
+    values.user = null
+  })
 
-netlifyIdentity.init()
+  netlifyIdentity.init()
 
-values.login = () => {
-  netlifyIdentity.open()
+  values.login = () => {
+    netlifyIdentity.open()
+  }
 }
 
 export default function useNetlifyIdentity(): {
