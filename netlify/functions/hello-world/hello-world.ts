c201864e7cab7d4a91fcbd93e042e9c4488c81ea
Robbie <robbiethewadley@gmail.com>
2023-04-28 13:12:00 -0600

    more api testing

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,12 +1,45 @@
-import { Handler } from '@netlify/functions'
+import { Handler } from "@netlify/functions"
+import { config } from "dotenv"
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai"
 
-export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
+config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+
+export const handler: Handler = async event => {
+  const { name = "stranger" } = event.queryStringParameters ?? {}
+
+  const prompt: ChatCompletionRequestMessage[] = [
+    {
+      role: "system",
+      content: `Say hi to ${name} by name, and generate a unique joke, one that's never been told before`,
+    },
+  ]
+
+  // ask openai for a random joke. use their gpt-3.5-turbo model
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: prompt,
+  })
+
+  const joke = completion.data.choices[0]?.message
+
+  if (!joke) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Sorry, ${name}, I couldn't think of a joke for you`,
+      }),
+    }
+  }
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${name}!`,
+      message: joke,
     }),
   }
 }
