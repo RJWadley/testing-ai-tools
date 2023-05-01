256f138a64bd28af8d3f6cf4f9ee0bd671f6b08c
Robbie <robbiethewadley@gmail.com>
2023-05-01 10:40:00 -0600

    object copy

--- src/utils/useNetlifyIdentity.ts
+++ src/utils/useNetlifyIdentity.ts
@@ -48,7 +48,7 @@ export default function useNetlifyIdentity(): {
     const listener = () => {
       setTimeout(() => {
         startTransition(() => {
-          setInternals(values)
+          setInternals({ ...values })
         })
       }, 1)
     }
